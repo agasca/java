0d7b6145312274f5680903164add4972d5166f19
zfa-u <zfa_@hotmail.com>
2019-11-09 08:15:31 -0600

    20191109 continuar

--- Arreglo.java
+++ Arreglo.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
+//
+/**
+ * Ficha tecnica in : -99 para salir : R22savcxmnb@ :
+ * R22sammnbvcx"mnbvcxzpytrwqmnbmnb@ : enteros out : operacion metodos :
+ * funciones : mensaje de error : notas : exceptions
+ * https://www.geeksforgeeks.org/types-of-exception-in-java-with-examples/
+ * enmascaramiento de entrada de línea de comando
+ * https://es.switch-case.com/53985162
+ */
+//
 class Arreglo {
-    // 20191109
     public static void main(String[] args) {
+        // Inicializa
+        Scanner agb_leer = new Scanner(System.in);
+        String valor = null;
         // Arreglos: tipos de declaracion
+        //incognitas
         String leyendasA[] = new String[9];
         leyendasA[1] = "a";
         leyendasA[2] = "b";
@@ -27,6 +42,26 @@ class Arreglo {
         default: // caso orden <> 2
             renglones = orden;
         }
+
+        imprimir("Ingreso de datos--");
+        for (int i = 1; i <= renglones; i++) {
+            for (int j = 1; j <= orden; j++) {
+                
+                System.out.print("Ingresa el valor para " + leyendasA[kIndiceLetraAB] + i + leyendasXY[kIndiceLetraAB] + " :");
+                valor = agb_leer.nextLine();
+                //System.out.println("->" + valor + "<-");
+                
+                if (j != orden) { System.out.print(" + "); }
+                
+                // salta letra a,b,..,z
+                kIndiceLetraAB++;
+            }
+            System.out.print(" = " + leyendasA[1 + orden] + i + "\n");
+            // asigna letra indice mas 1 para resultado
+            kIndiceLetraAB = 1;
+        }
+
+        imprimir("Despliega Ingreso de datos--");
         for (int i = 1; i <= renglones; i++) {
             for (int j = 1; j <= orden; j++) {
                 System.out.print(leyendasA[kIndiceLetraAB] + i + leyendasXY[kIndiceLetraAB]);
@@ -40,6 +75,11 @@ class Arreglo {
             // asigna letra indice mas 1 para resultado
             kIndiceLetraAB = 1;
         }
+    }
+
+    // m.imprimir(x)
+    public static void imprimir(String agbCadena) {
+        System.out.println("---------------------------------" + agbCadena);
     }
 }
 
