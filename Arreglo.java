61d36c7309b9adf6371ba1c5742144a41403617d
zfa-u <zfa_@hotmail.com>
2019-11-09 09:18:29 -0600

    20191109 beta01

--- Arreglo.java
+++ Arreglo.java
@@ -11,13 +11,17 @@ import java.util.Scanner;
  */
 //
 class Arreglo {
+    static int orden = 3, renglones = 0, kIndiceLetraAB = 1;
+    static String leyendasA[] = new String[9];
+    static String leyendasXY[] = new String[6];
+    static double datos[][] = new double[orden + 1][orden + 1];
+    static double valor = 0;
+    static Scanner leer = new Scanner(System.in);
+
     public static void main(String[] args) {
         // Inicializa
-        Scanner agb_leer = new Scanner(System.in);
-        String valor = null;
         // Arreglos: tipos de declaracion
-        //incognitas
-        String leyendasA[] = new String[9];
+        // incognitas
         leyendasA[1] = "a";
         leyendasA[2] = "b";
         leyendasA[3] = "c";
@@ -26,15 +30,13 @@ class Arreglo {
         leyendasA[6] = "f";
         leyendasA[7] = "g";
         leyendasA[8] = "h";
-        String leyendasXY[] = new String[6];
         leyendasXY[1] = "x";
         leyendasXY[2] = "y";
         leyendasXY[3] = "z";
         leyendasXY[4] = "w";
         leyendasXY[5] = "";
         // determinantes segun orden
-        int orden = 0, renglones = 0, kIndiceLetraAB = 1;
-        orden = 3;
+
         switch (orden) {
         case 2:
             renglones = (orden);
@@ -44,24 +46,24 @@ class Arreglo {
         }
 
         imprimir("Ingreso de datos--");
-        for (int i = 1; i <= renglones; i++) {
-            for (int j = 1; j <= orden; j++) {
-                
-                System.out.print("Ingresa el valor para " + leyendasA[kIndiceLetraAB] + i + leyendasXY[kIndiceLetraAB] + " :");
-                valor = agb_leer.nextLine();
-                //System.out.println("->" + valor + "<-");
-                
-                if (j != orden) { System.out.print(" + "); }
-                
-                // salta letra a,b,..,z
-                kIndiceLetraAB++;
-            }
-            System.out.print(" = " + leyendasA[1 + orden] + i + "\n");
-            // asigna letra indice mas 1 para resultado
-            kIndiceLetraAB = 1;
-        }
+        ingresaArreglo();
 
-        imprimir("Despliega Ingreso de datos--");
+        leer.close();
+
+        imprimir("Despliega ecuación--");
+        imprimeArreglo();
+        
+        imprimir("Despliega datos del arreglo--");
+        imprimeDatosArreglo();
+    }
+
+    // Metodos
+    // m.imprimir(x)
+    public static void imprimir(String agbCadena) {
+        System.out.println("---------------------------------" + agbCadena);
+    }
+
+    public static void imprimeArreglo() {
         for (int i = 1; i <= renglones; i++) {
             for (int j = 1; j <= orden; j++) {
                 System.out.print(leyendasA[kIndiceLetraAB] + i + leyendasXY[kIndiceLetraAB]);
@@ -77,9 +79,37 @@ class Arreglo {
         }
     }
 
-    // m.imprimir(x)
-    public static void imprimir(String agbCadena) {
-        System.out.println("---------------------------------" + agbCadena);
+
+    public static void imprimeDatosArreglo() {
+        for (int i = 1; i <= renglones; i++) {
+            for (int j = 1; j <= orden; j++) {
+                System.out.print(datos[i][j]);
+                if (j != orden) {
+                    System.out.print(" + ");
+                }
+                // salta letra a,b,..,z
+                kIndiceLetraAB++;
+            }
+            System.out.print(" = " + leyendasA[1 + orden] + i + "\n");
+            // asigna letra indice mas 1 para resultado
+            kIndiceLetraAB = 1;
+        }
+    }
+
+
+    public static void ingresaArreglo() {
+        for (int i = 1; i <= renglones; i++) {
+            for (int j = 1; j <= orden; j++) {
+                System.out.print(
+                        "Ingresa el valor para " + leyendasA[kIndiceLetraAB] + i + leyendasXY[kIndiceLetraAB] + " :");
+                valor = leer.nextDouble();
+                System.out.println("->" + valor + "<->" + i + j + "<-");
+                datos[i][j] = valor;
+                kIndiceLetraAB++;
+            }
+            // asigna letra indice mas 1 para resultado
+            kIndiceLetraAB = 1;
+        }
     }
 }
 
