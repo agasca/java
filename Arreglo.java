48110dabfd1a2d0b9bec8a2892f17e64993b542d
zfa-u <zfa_@hotmail.com>
2019-11-17 00:47:55 -0600

    20191116 delta general, x, y en orden 2 beta01

--- Arreglo.java
+++ Arreglo.java
@@ -9,14 +9,14 @@ import java.util.Scanner;
  */
 //
 class Arreglo {
-    static int orden = 2, renglones = 0, kIndiceLetraAB = 1;
-    static String leyendasA[] = new String[9];
-    static String leyendasXY[] = new String[6];
-    static double datos[][] = new double[orden + 1][orden + 1];
-    static double valor = 0;
-    static Scanner leer = new Scanner(System.in);
-    static double deltaG = 0, deltaX = 0, deltaY = 0, deltaPaso = 1, deltaSubtotal = 0;
-    static int direccionY = 0;
+    public static int orden = 2, renglones = 0, kIndiceLetraAB = 1;
+    public static String leyendasA[] = new String[9];
+    public static String leyendasXY[] = new String[6];
+    public static double datos[][] = new double[orden + 1][orden + 1];
+    public static double valor = 0;
+    public static Scanner leer = new Scanner(System.in);
+    public static double deltaG = 0, deltaX = 0, deltaY = 0, deltaPaso = 1, deltaSubtotal = 0, dCambio = 0;
+    public static int direccionY = 0;
 
     public static void main(final String[] args) {
         // Inicializa
@@ -56,8 +56,23 @@ class Arreglo {
         imprimir("Despliega datos del arreglo--imprimeDatosArreglo()");
         imprimeDatosArreglo();
 
-        imprimir("Despliega datos del arreglo--computoDatosArreglo()");
-        computoDatosArreglo();
+        imprimir("Despliega datos del arreglo--computoDatosArregloG()");
+        computoDatosArregloG();
+
+        deltaSubtotal = 0;
+        imprimir("Despliega datos del arreglo--computoDatosArregloX()");
+        computoDatosArregloX();
+
+        //imprimir("Despliega datos del arreglo--imprimeDatosArreglo() RESET");
+        //imprimeDatosArreglo();
+
+        deltaSubtotal = 0;
+        //dCambio = 0;
+        imprimir("Despliega datos del arreglo--computoDatosArregloY()");
+        computoDatosArregloY();
+
+        imprimir("Resultado x :" + (deltaX/deltaG) + " y : " + (deltaY/deltaG));
+
     }
 
     // Metodos
@@ -100,7 +115,7 @@ class Arreglo {
         }
     }
 
-    public static void computoDatosArreglo() {
+    public static void computoDatosArregloG() {
         // inicializa
         for (int i = 1; i <= renglones; i++) {
             for (int j = 1; j <= orden; j++) {
@@ -110,7 +125,7 @@ class Arreglo {
                 } else if (i == 2 && j == 2) {
                     direccionY -= 1;
                 }
-                //System.out.print(datos[i + direccionY][j]); // br
+                System.out.print(datos[i + direccionY][j]); // br
                 deltaPaso = deltaPaso * datos[i + direccionY][j];
                 if (j != orden) { // br
                     System.out.print(" * ");
@@ -122,18 +137,117 @@ class Arreglo {
             } else if (i == 2) {
                 direccionY += 1;
             }
-            if(i%2==0){
-                deltaPaso*=-1;
+            if (i % 2 == 0) {
+                deltaPaso *= -1;
             }
             deltaSubtotal += deltaPaso;
-            // System.out.print(" = " + datos[i + direccionY][0] + "\n"); // br
-            //System.out.print(" = " + deltaSubtotal + "\n"); // br
+            System.out.print(" = " + datos[i + direccionY][0] + "\n"); // br
+            // System.out.print(" > " + deltaSubtotal + "\n"); // br
             deltaPaso = 1;
             // asigna letra indice mas 1 para resultado
             kIndiceLetraAB = 1; // br
             deltaG = deltaSubtotal;
         }
         System.out.println("Resultado : " + deltaG);
+    }
+
+    public static void computoDatosArregloX() {
+        // inicializa
+        // intercambia valores desigualdad a X, utilizando dCambio
+        dCambio = datos[1][0]; // desigualdad fila 1
+        datos[1][0] = datos[1][1]; //resguarda para reset
+        datos[1][1] = dCambio;
+
+        dCambio = datos[2][0]; // desigualdad fila 2
+        datos[2][0] = datos[2][1]; //resguarda para reset
+        datos[2][1] = dCambio;
+
+        // proceso opera
+        for (int i = 1; i <= renglones; i++) {
+            for (int j = 1; j <= orden; j++) {
+                // Sí fila 1 y columna 2 entonces baja
+                if (i == 1 && j == 2) {
+                    direccionY += 1; // br
+                } else if (i == 2 && j == 2) {
+                    direccionY -= 1;
+                }
+                System.out.print(datos[i + direccionY][j]); // br
+                deltaPaso = deltaPaso * datos[i + direccionY][j];
+                if (j != orden) { // br
+                    System.out.print(" * ");
+                }
+                kIndiceLetraAB++; // salta letra a,b,..,z
+            }
+            if (i == 1) { // subi-baja
+                direccionY -= 1;
+            } else if (i == 2) {
+                direccionY += 1;
+            }
+            if (i % 2 == 0) {
+                deltaPaso *= -1;
+            }
+            deltaSubtotal += deltaPaso;
+            System.out.print(" = " + datos[i + direccionY][0] + "\n"); // br
+            // System.out.print(" > " + deltaSubtotal + "\n"); // br
+            deltaPaso = 1;
+            // asigna letra indice mas 1 para resultado
+            kIndiceLetraAB = 1; // br
+            deltaX = deltaSubtotal;
+        }
+        // intercambia valores desigualdad a X, utilizando dCambio Reset
+        dCambio = datos[1][0]; // desigualdad fila 1
+        datos[1][0] = datos[1][1]; //resguarda para reset
+        datos[1][1] = dCambio;
+
+        dCambio = datos[2][0]; // desigualdad fila 2
+        datos[2][0] = datos[2][1]; //resguarda para reset
+        datos[2][1] = dCambio;
+        
+        System.out.println("Resultado : " + deltaX);
+    }
+
+    public static void computoDatosArregloY() {
+        // inicializa
+        // intercambia valores desigualdad a Y, utilizando dCambio
+        dCambio = datos[1][0]; // desigualdad fila 1
+        datos[1][2] = dCambio;
+
+        dCambio = datos[2][0]; // desigualdad fila 2
+        datos[2][2] = dCambio;
+
+        // proceso opera
+        for (int i = 1; i <= renglones; i++) {
+            for (int j = 1; j <= orden; j++) {
+                // Sí fila 1 y columna 2 entonces baja
+                if (i == 1 && j == 2) {
+                    direccionY += 1; // br
+                } else if (i == 2 && j == 2) {
+                    direccionY -= 1;
+                }
+                System.out.print(datos[i + direccionY][j]); // br
+                deltaPaso = deltaPaso * datos[i + direccionY][j];
+                if (j != orden) { // br
+                    System.out.print(" * ");
+                }
+                kIndiceLetraAB++; // salta letra a,b,..,z
+            }
+            if (i == 1) { // subi-baja
+                direccionY -= 1;
+            } else if (i == 2) {
+                direccionY += 1;
+            }
+            if (i % 2 == 0) {
+                deltaPaso *= -1;
+            }
+            deltaSubtotal += deltaPaso;
+            System.out.print(" = " + datos[i + direccionY][0] + "\n"); // br
+            // System.out.print(" > " + deltaSubtotal + "\n"); // br
+            deltaPaso = 1;
+            // asigna letra indice mas 1 para resultado
+            kIndiceLetraAB = 1; // br
+            deltaY = deltaSubtotal;
+        }
+        System.out.println("Resultado : " + deltaY);
     }
 
     public static void ingresaArreglo() {
